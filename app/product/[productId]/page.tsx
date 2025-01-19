02bdc4f605ddf80c3b50ac7c4a6a01a1f17af004
Juan Vianney NM <juanvianneynm@gmail.com>
2025-01-19 19:15:34 +0100

    bug-fix

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -107,7 +107,7 @@ export default function Page({params} : {params: Promise<{productId: string}>}) 
                         <div className="flex items-center gap-2">
                             <SelectSizeInput sizes={product.variants[0].sizes} handleChange={setSize} />
                         </div>
-                        <div className="flex flex-col">
+                        <div className="flex flex-col border-b-2 border-b">
                             {product.description.map((d: {name: string, value: {name: string, value: string}[]}, index) => {
                                 // setDescriptionsIsOpen([...descriptionsIsOpen, false]);
                                 return (
@@ -115,7 +115,7 @@ export default function Page({params} : {params: Promise<{productId: string}>}) 
                                         <button 
                                             key={d.name} 
                                             onClick={()=>setDescriptionsIsOpen({...descriptionsIsOpen, [d.name.toLowerCase()]: !descriptionsIsOpen[d.name.toLowerCase()]})}
-                                            className={`font-semibold text-lg flex justify-between items-center py-4 px-3 border-t-2 ${index === product.description.length - 1 ? "border-b-2 border-b" : ""}`}>
+                                            className={`font-semibold text-lg flex justify-between items-center py-4 px-3 border-t-2`}>
                                             {d.name}
                                             <svg 
                                                 // style={{transform: `rotate(${descriptionsIsOpen[d.name.toLowerCase()] ? "180deg": "0"})`}}
@@ -141,9 +141,9 @@ export default function Page({params} : {params: Promise<{productId: string}>}) 
                         </div>
                         <div className="w-full flex gap-1">
                             <button onClick={addToCart} className="w-full p-2 border border-black bg-black text-white font-semibold transition">Ajouter au panier</button>
-                            <button className="w-12 h-12 flex justify-center items-center p-2 border border-black">
+                            {/* <button className="w-12 h-12 flex justify-center items-center p-2 border border-black">
                                 <svg viewBox="0 0 24 24" width="1em" height="1em" fill="currentColor" aria-labelledby="wishlist-:R151bk58cap:" focusable="false" aria-hidden="false" role="img"><title id="wishlist-:R151bk58cap:">Wishlist</title><path d="M17.488 1.11h-.146a6.55 6.55 0 0 0-5.35 2.81A6.57 6.57 0 0 0 6.62 1.116 6.406 6.406 0 0 0 .09 7.428c0 7.672 11.028 15.028 11.497 15.338a.75.75 0 0 0 .826 0c.47-.31 11.496-7.666 11.496-15.351a6.43 6.43 0 0 0-6.42-6.306M12 21.228C10.018 19.83 1.59 13.525 1.59 7.442c.05-2.68 2.246-4.826 4.934-4.826h.088c2.058-.005 3.93 1.251 4.684 3.155.226.572 1.168.572 1.394 0 .755-1.907 2.677-3.17 4.69-3.16h.02c2.7-.069 4.96 2.118 5.01 4.817 0 6.089-8.429 12.401-10.41 13.8"></path></svg>
-                            </button>
+                            </button> */}
                         </div>
                     </div>
                 </div>
